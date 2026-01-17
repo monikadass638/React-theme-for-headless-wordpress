0565abc2e4a6ce45d082d9ee2b47518a9a7a05f8
monika638 <monika638@gmail.com>
2026-01-17 11:47:02 +1100

    added a featured image on the blogsingle project

--- src/pages/BlogSingle.jsx
+++ src/pages/BlogSingle.jsx
@@ -7,11 +7,16 @@ import { getPostBYslug } from '../api/wordpress'
 function BlogSingle() {
     const { slug } = useParams();
     const [singlePost, setsinglePost] = useState(null);
+    const media = singlePost && singlePost[0]?._embedded?.["wp:featuredmedia"]?.[0];
+    const image =
+
+        media?.media_details?.sizes?.large?.source_url;
     useEffect(() => {
         const fetchPost = async () => {
             const data = await getPostBYslug(slug);
             setsinglePost(data);
-            console.log(singlePost);
+
+
         };
         fetchPost();
     }, [slug]);
@@ -21,6 +26,7 @@ function BlogSingle() {
             <Header />
             <div className='container my-5'>
                 <h2>{singlePost && singlePost[0]?.title?.rendered}</h2>
+                <div className='my-4 post-img-featured'> {image && < a href={singlePost.link}><img src={image} alt={media?.alt_text || "Blog image"} /></a>}</div>
                 <p className='mt-3' dangerouslySetInnerHTML={{ __html: singlePost && singlePost[0]?.content?.rendered }}></p>
             </div>
             <Footer />
