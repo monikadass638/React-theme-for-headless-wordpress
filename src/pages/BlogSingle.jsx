c67611ffa729e2b99a556017aab5c661ed76a492
monika638 <monika638@gmail.com>
2026-01-19 14:29:49 +1100

    added category name in single blog page

--- src/pages/BlogSingle.jsx
+++ src/pages/BlogSingle.jsx
@@ -7,6 +7,16 @@ import { getPostBYslug } from '../api/wordpress'
 function BlogSingle() {
     const { slug } = useParams();
     const [singlePost, setsinglePost] = useState(null);
+
+    const formattedDate = new Date(singlePost && singlePost[0]?.date).toLocaleDateString("en-AU", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+
+    const CategoryName = singlePost && singlePost[0]?._embedded?.["wp:term"][0]?.map((cat) => cat.name).join(" - ");
+    //console.log(CategoryName);
+
     const media = singlePost && singlePost[0]?._embedded?.["wp:featuredmedia"]?.[0];
     const image =
 
@@ -26,8 +36,12 @@ function BlogSingle() {
         <>
             <Header />
             <div className='container my-5'>
-                <h2>{singlePost && singlePost[0]?.title?.rendered}</h2>
-                <div className='my-4 post-img-featured'> {image && < a href={singlePost.link}><img src={image} alt={media?.alt_text || "Blog image"} /></a>}</div>
+                <h6>{CategoryName}</h6>
+                <h1>{singlePost && singlePost[0]?.title?.rendered}</h1>
+                <div className='text-muted fs-6'>
+                    <div>{singlePost && singlePost[0]?._embedded?.author[0]?.name} </div>
+                    <div>Published on {formattedDate}</div> </div>
+                <div className='my-4 post-img-featured'> {image && < a href={singlePost?.[0]?.link}><img src={image} alt={media?.alt_text || "Blog image"} /></a>}</div>
                 <p className='mt-3' dangerouslySetInnerHTML={{ __html: singlePost && singlePost[0]?.content?.rendered }}></p>
             </div>
             <Footer />
